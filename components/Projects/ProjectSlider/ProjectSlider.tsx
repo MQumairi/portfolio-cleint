114bb1d7691fce06009a552b94f2f40f51b18424
Mohammed Alqumairi <moh.alqumairi@gmail.com>
2021-08-21 22:00:36 +0100

    byshot added

--- components/Projects/ProjectSlider/ProjectSlider.tsx
+++ components/Projects/ProjectSlider/ProjectSlider.tsx
@@ -10,19 +10,30 @@ const ProjectSlider = ({ projects }) => {
         title: { rendered: "Ticket System" },
         acf: {
           priority: 1,
-          raw_description:
-            "Full stack ticket system, built with .NET Core 3.1 in the backend, and React with Hooks in the frontend. Published on Azure.",
+          raw_description: "Full stack ticket system, built with .NET Core 3.1 in the backend, and React with Hooks in the frontend. Published on Azure.",
           live_link: "https://mqumairi-tickets.azurewebsites.net/",
           github_link: "https://github.com/MQumairi/ticket-system-server",
           screenshot: "/tickets.jpg",
         },
       },
+
       {
-        title: { rendered: "Invexel" },
+        title: { rendered: "ByShot" },
         acf: {
           priority: 2,
           raw_description:
-            "Real estate investment platform built using Php. Mobile responsive, and with functionality to subscribe users to email lists.",
+            "Native macOS app developed using Swift/AppKit. ByShot allows the user to extract editable text from images and videos using OCR technology.",
+          live_link: "https://apps.apple.com/us/app/byshot/id1581411585",
+          github_link: "https://github.com/MQumairi/ByShot-Public/blob/master/README.md",
+          screenshot: "/byshot.jpg",
+        },
+      },
+
+      {
+        title: { rendered: "Invexel" },
+        acf: {
+          priority: 3,
+          raw_description: "Real estate investment platform built using Php. Mobile responsive, and with functionality to subscribe users to email lists.",
           live_link: "https://invexel.com/",
           github_link: "",
           screenshot: "/invexel.jpg",
@@ -31,31 +42,18 @@ const ProjectSlider = ({ projects }) => {
       {
         title: { rendered: "Budget Buddies" },
         acf: {
-          priority: 3,
-          raw_description:
-            "Led a team of developers in the 48in48 hackathon to develop a custom theme for the non-profit, Budget Buddies.",
+          priority: 4,
+          raw_description: "Led a team of developers in the 48in48 hackathon to develop a custom theme for the non-profit, Budget Buddies.",
           live_link: "https://budgetbuddies.org/",
           github_link: "",
           screenshot: "/budgetbuddies.jpg",
         },
       },
       {
-        title: { rendered: "Haskell LinReg" },
-        acf: {
-          priority: 4,
-          raw_description:
-            "A tool for solving regression problems, using linear regression and gradient descent. Includes a file parser, and feature scaling.",
-          live_link: "",
-          github_link: "https://github.com/MQumairi/Haskell-LinReg",
-          screenshot: "/linreg.jpg",
-        },
-      },
-      {
         title: { rendered: "Numpy NN" },
         acf: {
           priority: 5,
-          raw_description:
-            "Using just the Numpy library, developed a fully functioning neural network, able to classify the MNIST dataset with 91% accuracy.",
+          raw_description: "Using just the Numpy library, developed a fully functioning neural network, able to classify the MNIST dataset with 91% accuracy.",
           live_link: "",
           github_link: "https://github.com/MQumairi/numpy-neural-network",
           screenshot: "/numpynn.jpg",
@@ -65,9 +63,7 @@ const ProjectSlider = ({ projects }) => {
   }
   //A function that takes a priority, and returns a Project
   const getProject = (priortiy: number) => {
-    const foundProject = projects.find(
-      (project) => project["acf"]["priority"] == priortiy
-    );
+    const foundProject = projects.find((project) => project["acf"]["priority"] == priortiy);
 
     return foundProject;
   };
@@ -116,10 +112,7 @@ const ProjectSlider = ({ projects }) => {
 
           return (
             <li key={i} className={activeStlye}>
-              <a
-                className={styles.pageLink}
-                onClick={() => setActiveProject(getProject(i + 1))}
-              >
+              <a className={styles.pageLink} onClick={() => setActiveProject(getProject(i + 1))}>
                 {i + 1}
               </a>
             </li>
